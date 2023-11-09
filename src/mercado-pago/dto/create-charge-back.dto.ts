f1d3e468dee1ce8a5d00d80dce7c6962c8246855
rod <rodrigos_ars@hotmail.com>
2023-11-08 22:18:31 -0300

    feat: doc in create charge back too

--- src/mercado-pago/dto/create-charge-back.dto.ts
+++ src/mercado-pago/dto/create-charge-back.dto.ts
@@ -1,3 +1,10 @@
-export class CreateChargeBackDto{      
+import { ApiProperty } from "@nestjs/swagger"
+
+export class CreateChargeBackDto{   
+    
+    @ApiProperty({
+        description: 'OPTIONAL - Refund amount. If the property (amount) is removed from the body, it will create a full refund.',
+        example: 'amount: 100',
+    })
     amount?: number
 }
