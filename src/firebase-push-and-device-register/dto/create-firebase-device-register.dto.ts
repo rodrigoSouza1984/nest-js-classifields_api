2b249dabc6863abffb081a03e7d3f461ae9554aa
rod <rodrigos_ars@hotmail.com>
2023-12-11 21:20:36 -0300

    feat: add swager in register token body

--- src/firebase-push-and-device-register/dto/create-firebase-device-register.dto.ts
+++ src/firebase-push-and-device-register/dto/create-firebase-device-register.dto.ts
@@ -1,7 +1,19 @@
 import { User } from "src/user/entities/user.entity";
+import { ApiProperty } from "@nestjs/swagger"
 
-export class CreateFirebaseDeviceRegisterDto {    
+export class CreateFirebaseDeviceRegisterDto {  
+    
+    @ApiProperty({
+        description: 'REQUIRED - user id add token in data base',
+        example: `userId: number` ,
+    })
     userId?: number;    
+
+    @ApiProperty({
+        description: 'REQUIRED - token add in data base',
+        example: `token: string` ,
+    })
     token?: string;
+    
     user?: User;
 }
